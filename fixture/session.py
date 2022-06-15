38bca024de40ce6db2d048a5317e6fd33224cd14
kampik <sineguboff@gmail.com>
2022-06-15 23:00:56 +0700

    first commit

---
+++ fixture/session.py
@@ -0,0 +1,44 @@
+from selenium.webdriver.common.by import By
+
+
+class SessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, username, password):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element(by=By.NAME, value="user").clear()
+        wd.find_element(by=By.NAME, value="user").send_keys(username)
+        wd.find_element(by=By.NAME, value="pass").clear()
+        wd.find_element(by=By.NAME, value="pass").send_keys(password)
+        wd.find_element(by=By.XPATH, value="//input[@value='Login']").click()
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element(by=By.LINK_TEXT, value="Logout").click()
+        wd.find_element(by=By.NAME, value="user")
+
+    def ensure_logout(self):
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements(by=By.LINK_TEXT, value="Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element(by=By.XPATH, value="//div[@id='top']/form/b").text[1:-1]
+
+    def ensure_login(self, username, password):
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
