d699c08f1a3fe89906e3b9ad6145127dba9ac0b0
Pavel Sinegubov <p.sinegubov@bi.zone>
2022-06-21 18:03:13 +0700

    fix soap methods ans tests

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,10 +1,10 @@
 
 
 def test_add_project(app, json_projects):
-    old_projects_list = app.project.get_projects_list()
+    old_projects_list = app.soap.get_projects_list()
     project = json_projects
     app.project.add_new_project(project)
-    new_projects_list = app.project.get_projects_list()
+    new_projects_list = app.soap.get_projects_list()
     assert len(old_projects_list) + 1 == len(new_projects_list)
     old_projects_list.append(project)
     assert sorted(old_projects_list, key=(lambda x: x.project_name)) == \
